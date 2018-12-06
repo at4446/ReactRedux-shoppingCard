0d8fc886bd06efb68c6bf74c9c40f9ff3d744610
Amir <at4446@mun.ca>
2018-12-05 20:17:33 -0500

    Pushing semi-funtional part of the app for back up and tracking

---
+++ src/components/TaxesFees/TaxesFees.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Row, Col, Tooltip, OverlayTrigger} from 'react-bootstrap/lib';
+
+export default class TaxesFees extends Component{
+
+	render(){
+		return(
+
+			<Row className="show-grid">
+				<Col md={6}>Taxes</Col>
+				<Col md={6}>{this.props.taxes}</Col>
+			</Row>
+
+			)
+
+
+	}
+
+
+
+}
