3e7e11ce01f7e67f3de61d5600efbc16c440a4b5
Amir <at4446@mun.ca>
2018-12-05 20:30:54 -0500

    Pushing semi-funtional part of the app for back up and tracking

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import store from './store';
 ReactDOM.render(
- <Provider store={store }>
+ <Provider store={store  }>
 	<App />
   </Provider>, document.getElementById('root'));
 
